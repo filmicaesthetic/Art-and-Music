72088fa82fd481a2891a8f28da6c6392bdc0a4c7
Dan <96343425+filmicaesthetic@users.noreply.github.com>
2022-04-01 23:33:30 +0100

    Visualisations

--- scripts/Guitar Tab Extract.R
+++ scripts/Guitar Tab Extract.R
@@ -7,10 +7,7 @@ library(stringr)
 library(tidyr)
 library(showtext)
 library(gganimate)
-
-# import font from Google
-font_add_google("Red Hat Mono", "Red Hat Mono")
-showtext_auto()
+library(forcats)
 
 # function for scraping text from webpage
 scrape_data <- function (x) {
@@ -18,9 +15,6 @@ scrape_data <- function (x) {
     html_text()
   return(y)
 }
-
-# capo value to be added
-capo <- 5
 
 # octave of notes
 notes <- data.frame(notes = c("C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"))
@@ -36,19 +30,32 @@ guitar_strings <- data.frame(notes = c("E", "A", "D", "G", "B", "E"),
                              octave_id = c(2, 2, 3, 3, 3, 4)) %>% 
   left_join(full_scale, by = c("notes", "octave_id"))
 
+
+# url of tab
+tab_url <- "https://www.guitartabs.cc/tabs/j/jeff_buckley/hallelujah_tab_ver_7.html"
+
 # scrape data from guitartabs.cc
-hallelujah <- scrape_data("https://www.guitartabs.cc/tabs/j/jeff_buckley/hallelujah_tab_ver_7.html")
+hallelujah <- scrape_data(tab_url)
 
 # extract tab section
-hallelujah_text <- sub('\\\tComments.*', "", sub('.*Artist:', "", hallelujah))
+hallelujah_text <- sub('\\\tComments.*', "", sub('.*show chord diagrams', "", hallelujah))
 # extract individual strings
-hallelujah_tab <- str_match_all(hallelujah_text, "\\\n\\|\\s*(.*?)\\s*\\|\\\r")
+hallelujah_tab <- str_match_all(hallelujah_text, "\\\n(.*?)\\-\\s*(.*?)\\s*\\|\\\r")
+# extract capo
+capo_text <- str_match_all(hallelujah_text, "Capo*(.*?)\\s*\\\r")
+
+if (is.na(str_count(capo_text[[1]][1])) == FALSE) {
+  capo <- as.numeric(str_extract(capo_text,"\\(?[0-9,.]+\\)?"))} else { 
+    capo <- 0}
+
+#count of tab rows
+tab_count <- nrow(hallelujah_tab[[1]]) / 6
 # combine with notes and octave data
 hallelujah_df <- data.frame(tabs = hallelujah_tab[[1]][1:nrow(hallelujah_tab[[1]])],
-           notes = rep(c("E", "A", "D", "G", "B", "E"), nrow(hallelujah_tab[[1]]) / 6),
-           string = rep(c("1", "2", "3", "4", "5", "6"), nrow(hallelujah_tab[[1]]) / 6),
-           tab_row = (rep(c(seq(1:23)), each = 6)),
-           octave_id = rep(c(2, 2, 3, 3, 3, 4), nrow(hallelujah_tab[[1]]) / 6))
+           notes = rep(c("E", "A", "D", "G", "B", "E"), tab_count),
+           string = rep(c("1", "2", "3", "4", "5", "6"), tab_count),
+           tab_row = (rep(c(seq(1:tab_count)), each = 6)),
+           octave_id = rep(c(2, 2, 3, 3, 3, 4), tab_count))
 
 # separate out sections
 intro <- hallelujah_df[1:36,] %>% mutate(section = "intro")
@@ -60,7 +67,7 @@ solo <- hallelujah_df[97:120,] %>% mutate(section = "solo")
 outro <- hallelujah_df[121:138,] %>% mutate(section = "outro")
 
 # combine sections into one df
-whole_song <- rbind(intro, verse, chorus, verse, chorus, verse, chorus, verse, chorus, bridge, interlude, solo, outro)
+whole_song <- rbind(intro, verse, chorus, bridge, interlude, solo, outro)
 # extract individual notes and calculate actual note played
 whole_song_long <- whole_song %>% 
   mutate(tab_raw = tabs,
@@ -76,57 +83,4 @@ whole_song_long <- whole_song %>%
   select(-notes, -octave_id) %>%
   left_join(full_scale, by = "id")
 
-# polar plot notes
-whole_song_long %>%
-  group_by(notes, octave_id, section) %>%
-  summarise(n = n()) %>%
-  ggplot(aes(x = notes, y = octave_id)) +
-  geom_point(aes(size = n, color = section), alpha = 0.35, shape = 1, stroke = 3) +
-  scale_size(range = c(1, 50)) +
-  ylim(c(1, 5)) +
-  coord_polar() +
-  theme_minimal() +
-  ggtitle("Hallelujah", subtitle = "by Jeff Buckley") +
-  labs(y = "OCTAVE", x = "", color = "SECTION", caption = "data: guitartabs.cc") +
-  guides(size = "none", color = guide_legend(title.position="top", title.hjust = 0.5)) +
-  theme(legend.position = "bottom",
-        text = element_text(family = "Red Hat Mono", size = 35),
-        axis.title.y = element_text(face = "bold", size = 30, color = "#a87986", hjust = 0.72),
-        plot.title = element_text(hjust = 0.5, size = 65),
-        plot.subtitle = element_text(hjust = 0.5, size = 40),
-        legend.title = element_text(face = "bold", size = 30, color = "#a87986", hjust = 0.72))
 
-# save plot
-ggsave("hallelujah.png", width = 6, height = 8, dpi = 300)
-
-# add strings for zero values
-
-# animate strings
-a <- whole_song_long %>%
-  select(pos_id, string, tabs, tab_row, notes, section) %>%
-  mutate(tabs = as.numeric(gsub(0, NA, tabs))) %>%
-  ggplot(aes(x = string, y = tabs)) +
-  geom_point(aes(color = notes), size = 20, alpha = 0.65, shape = 1, stroke = 8) +
-  geom_col(aes(y = 12, fill = notes), width = 0.04, alpha = 0.65) +
-  transition_states(pos_id, transition_length = 2) +
-  shadow_wake(wake_length = 0.03) +
-  exit_fade() +
-  #scale_y_continuous(breaks = seq(1:max(whole_song_long$tabs)), limits = c(0, 12)) +
-  scale_y_reverse(breaks = seq(max(whole_song_long$tabs):1), limits = c(12, 0)) +
-  theme_minimal() +
-  labs(y = "FRET", x = "", color = "NOTE\n") +
-  guides(size = "none", fill = "none", color = guide_legend(title.position="top", title.hjust = 0.5)) +
-  theme(legend.position = "right",
-        text = element_text(family = "Red Hat Mono", size = 25),
-        axis.title.y = element_text(face = "bold", size = 20, color = "#a87986", hjust = 0.5),
-        plot.title = element_text(hjust = 0.5, size = 40),
-        panel.grid.major.x = element_line(size = 3),
-        panel.grid.minor.y = element_blank(),
-        legend.key.height = unit(100, "pt"),
-        plot.subtitle = element_text(hjust = 0.5, size = 30),
-        legend.text = element_text(size = 30),
-        legend.title = element_text(face = "bold", size = 20, color = "#a87986", hjust = 0.5))
-
-# save animation
-animate(a, height = 1800, width = 800, fps = 6, duration = 60)
-anim_save("outputs/hallelujah.gif")
